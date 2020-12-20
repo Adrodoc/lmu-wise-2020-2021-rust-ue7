5dd2d94a824dd5a02415433efc3be26ce9b248f0
Adrodoc <adrodoc55@googlemail.com>
2020-12-20 15:23:10 +0100

    Exercise 7-1a & 7-1b

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    let body = fetch_crateinfo("reqwest").await?;
+    println!("body = {:?}", body);
+    Ok(())
+}
+
+async fn fetch_crateinfo(c: &str) -> Result<String, String> {
+    let url = "https://crates.io/api/v1/crates/".to_owned() + c;
+    let result = fetch_url(&url).await;
+    result.map_err(|error| format!("error code: {}", error))
+}
+
+async fn fetch_url(url: &str) -> Result<String, Error> {
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+    let response = client.get(url).send().await?;
+    response.text().await
 }
